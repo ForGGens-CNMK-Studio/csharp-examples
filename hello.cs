70ac3660131887f6367c6f1b4754bd6f2a1e87bf
UniveralFox <85844691+somedude12345@users.noreply.github.com>
2024-06-14 14:41:37 +0000

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,21 @@
+ using System;
+ class HelloWorld {
+   static void Main() {
+     Console.WriteLine("Hello World!");
+     int a = 10;
+     Console.WriteLine(a);
+     Console.WriteLine($"a = {a}");
+   }
+ }
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
