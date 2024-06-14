425b1056ef5eb3b7a31227597440255cea340992
cucoanes maria <cucoanesmaria@gmail.com>
2024-06-14 17:39:47 +0300

    primul exercitiu

---
+++ cucoanes maria/MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a = 10;
+            Console.WriteLine(a);
+            Console.WriteLine($"a ={a}");
+        }
+    }
+}
