0762c11095a844c6139970b4263a466c9de192a6
silviucelbarbos <silviupetrica2007@gmail.com>
2024-06-14 17:39:19 +0300

    primul exercitiu

---
+++ New folder/silviu/My first app/Program.cs
@@ -0,0 +1,14 @@
+﻿        namespace My_first_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int a = 10;
+            Console.WriteLine(a);
+            Console.WriteLine($"a = {a}");
+        }
+    }
+}
