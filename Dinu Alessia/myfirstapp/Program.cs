b2e18da78046c0df14bd8b5091194cc657a710f8
Alessia <alessia.claudia13@gmail.com>
2024-06-14 17:39:44 +0300

    primu exercitiu

---
+++ Dinu Alessia/myfirstapp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace myfirstapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World?");
+
+            int a = 10;
+            Console.WriteLine(a);
+            Console.WriteLine($"a={a}");
+        }
+    }
+}
