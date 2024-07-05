432945a0bdbc22d05315da8730950bab8b7a1251
silviucelbarbos <silviupetrica2007@gmail.com>
2024-07-05 17:55:23 +0300

    Ma doare capul rau,dar foarte fain de abia astept sa vad cat de complicat va ajunge

---
+++ New folder/silviu/csharp/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person p = new person("John", 30);
+
+            p.SayHello();
+
+            p.Name = "Jane";
+            p.Age = 25;
+
+            p.SayHello();
+
+            Console.WriteLine("Name: " + p.Name);
+            person[] people = GeneratePeople(5);
+            foreach (person person in people)
+            {
+                person.SayHello();
+            }
+        }
+
+
+
+        static public person[] GeneratePeople(int n)
+        {
+            person[] people = new person[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                people[i] = new person("Person" + i, i);
+            }
+            return people;
+            
+        }
+    }
+ 
+}
