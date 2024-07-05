ddbf03932c6b1eb0507ed28f76a2c17e0083538c
Cristian Iordachescu <cristian.iordachescu@ivfuture.uk>
2024-07-05 17:57:47 +0300

    Add initial project setup with basic OOP demonstration
    - Added a comprehensive .gitignore file to exclude various files and directories generated by Visual Studio, .NET Core, and other related tools and extensions.
    - Created Program.cs with a Program class demonstrating basic OOP in C#.
    - Added csharp-syntax.csproj to define project configuration, including target framework net8.0 and output type Exe.
    - Created csharp-syntax.sln to define the Visual Studio solution configuration.
    - Added person.cs with a Person class that includes properties for name and age, along with methods to manipulate these properties and print a greeting message.

---
+++ Iordachescu Cristian Stefan/csharp-syntax/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace csharp_syntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person p = new person("John", 30);
+
+            p.SayHello();
+
+            p.Name = "Jane";
+            p.Age = 25;
+
+            p.SayHello();
+
+            Console.WriteLine("Name: " + p.Name);
+
+            person[] people = GeneratePeople(5);
+            foreach (person person in people)
+            {
+                person.SayHello();
+            }
+            // compare with simple for
+            //for (int i = 0; i < people.Length; i++)
+            //{
+            //    people[i].SayHello();
+            //}
+        }
+
+        static public person[] GeneratePeople(int n)
+        {
+            person[] people = new person[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                people[i] = new person("Person " + i, i);
+            }
+
+            return people;
+        }
+    }
+}
